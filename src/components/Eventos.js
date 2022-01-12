f2f1c17cbe8cc0b26f19442b039208a66b8a1d8f
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2022-01-12 19:42:56 -0300

    Adicionando butões com eventos por props

--- src/components/Eventos.js
+++ src/components/Eventos.js
@@ -1,12 +1,21 @@
+import Button from './evento/Button'
+
 function Eventos() {
-  function clicar() {
-    alert('Você clicou!')
+  function clicar1() {
+    alert('Ativando o primeiro evento!')
   }
+
+  function clicar2(){
+    alert('Ativando o segundo evento!')
+  }
+
 
   return (
     <div>
       <p>Clique no botão abaixo!</p>
-      <button onClick={clicar}>Clique</button>
+      <Button event={clicar1} text="Primeiro evento"/>
+      <Button event={clicar2} text="Segundo evento"/>
+
     </div>
   )
 }
