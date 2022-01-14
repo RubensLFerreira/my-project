db61cde06445677252ec08886a2cfa22112840cb
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2022-01-14 20:03:18 -0300

    Aprendendo a utilizar o Router e Routes

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,5 +1,18 @@
+import { FaFreeCodeCamp } from "react-icons/fa";
+import { FaRedditAlien } from "react-icons/fa";
+import { FaCode } from "react-icons/fa";
+import styles from './Footer.module.css'
+
 function Footer() {
-  return <footer>Rodapé</footer>
+  return (
+    <footer>
+      <ul className={styles.social_list}>
+        <li><FaFreeCodeCamp /></li>
+        <li><FaRedditAlien /></li>
+        <li><FaCode /></li>
+      </ul>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
