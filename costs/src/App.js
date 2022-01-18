1912c82e678b3ac492d03b1ede3fed113467303f
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2022-01-18 20:47:32 -0300

    Adicionando os links navegaveis

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,8 +1,30 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Home } from './components/pages/Home';
+import { Company } from './components/pages/Company';
+import { Contacts } from './components/pages/Contacts';
+import { NewProject } from './components/pages/NewProject';
+
+import { Container } from './components/layout/Container';
+
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/contacts">Contatos</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </div>
+      <Routes>
+        <Container customClass="min-height">
+          <Route exact path="/" element={<Home />} /> 
+          <Route path="/company" component={<Company />} />
+          <Route path="/contacts" component={<Contacts />} />
+          <Route path="/newproject" component={<NewProject />} />
+        </Container>
+      </Routes>
+      <p>Footer</p>
+    </Router>
   );
 }
 
